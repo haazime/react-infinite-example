d9b816bcf16d07502757960d2a2e432fea8e0a36
haazime <h4zime@gmail.com>
2016-04-17 13:43:46 +0900

    classify

--- components/ItemSummary.js
+++ components/ItemSummary.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Card, CardMedia, CardTitle } from 'material-ui'
 
-const ItemSummary = ({ index, width }) => (
-  <Card style={{width: width}}>
-    <CardMedia>
-      <img src="/images/photo1.jpg" width={width} />
-    </CardMedia>
-    <CardTitle title={`#${index}`} subtitle={`Subtitle - ${index}`} />
-  </Card>
-)
+class ItemSummary extends React.Component {
+  render() {
+    const { width, index } = this.props
+    return (
+      <Card style={{width: width}}>
+        <CardMedia>
+          <img src="/images/photo1.jpg" width={width} />
+        </CardMedia>
+        <CardTitle title={`#${index}`} subtitle={`Subtitle - ${index}`} />
+      </Card>
+    )
+  }
+}
 
 export default ItemSummary
